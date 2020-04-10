0d14fbdbd8941e635eccee8c87310cd9989cc80d
fairmentat <fairmentat@mail.ru>
2020-04-10 21:18:49 +0400

    ready to git

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBOutlet weak var celsiusLabel:UILabel!
+    @IBOutlet weak var fahrenheitLabel:UILabel!
+    @IBOutlet weak var slider:UISlider!{
+        didSet{
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+          
+        }
+    }
+    
+    @IBAction func sliderValueChange(_ sender: UISlider){
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        
+        let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+    
+    
 
 }
 
+}
